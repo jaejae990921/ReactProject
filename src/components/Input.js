361eb9bfec2be4f2865177cb39377675648d73fc
jaejae990921 <jaejae990921@naver.com>
2023-10-13 00:02:06 +0900

    feat - input 버튼 및 svg 기능 추가
    - input 값에 따라 버튼 disabled 및 색상 변경
    - input 값에 따라 svg 색상 변경

--- src/components/Input.js
+++ src/components/Input.js
@@ -2,17 +2,16 @@ import { useState } from 'react';
 import styled from 'styled-components';
 
 const Stdiv = styled.div`
-  background-color: #353540;
+  background-color: #41414e;
   border-radius: 15px;
   display: flex;
-  width: 45%;
+  width: 750px;
   height: 60px;
   padding: 0 10px;
   align-items: center;
   justify-content: space-between;
   position: absolute;
-  bottom: 5%;
-  left: 30%;
+  bottom: 30px;
 `;
 
 const Stinput = styled.input`
@@ -25,8 +24,6 @@ const Stinput = styled.input`
   font-size: 15px;
 `;
 
-let isInput = false;
-
 const Stbutton = styled.button`
   border: none;
   border-radius: 10px;
@@ -35,21 +32,28 @@ const Stbutton = styled.button`
   display: flex;
   align-items: center;
   justify-content: center;
-  /* background-color: ${isInput ? 'rgb(25, 195, 125)' : 'transparent'}; */
-  background-color: rgb(25, 195, 125);
+  background-color: ${(props) =>
+    props.isInput ? 'rgb(25, 195, 125)' : 'transparent'};
+  transition: background-color 0.3s;
 `;
 
-export default function Input() {
+const Stsvg = styled.svg`
+  width: 20px;
+  height: 20px;
+  fill: ${(props) => (props.isInput ? 'white' : 'rgb(107,108,123)')};
+  transition: fill 0.3s;
+`;
+
+export default function Input({ addMsg }) {
   const [input, setInput] = useState('');
   const [response, setResponse] = useState('');
+  const [isInput, setIsInput] = useState(false);
 
-  const API_KEY = '123';
-  // const API_KEY = process.env.REACT_APP_API_KEY;
+  const API_KEY = process.env.REACT_APP_API_KEY;
 
   const sendMessage = async (e) => {
     e.preventDefault();
-
-    console.log('버튼 눌림!!');
+    addMsg(input);
 
     // 호출할 ChatGPT API 엔드포인트와 API 키 설정
     const apiUrl = 'https://api.openai.com/v1/chat/completions';
@@ -86,16 +90,15 @@ export default function Input() {
   const inputChange = (e) => {
     setInput(e.target.value);
     if (e.target.value.length == 0) {
-      isInput = false;
+      setIsInput(false);
     } else {
-      isInput = true;
+      setIsInput(true);
     }
-    isInputChange();
   };
 
-  const isInputChange = () => {
-    // 색 변경
-    console.log(isInput);
+  // 버튼 클릭 테스트
+  const testButton = () => {
+    console.log('버튼누림');
   };
 
   return (
@@ -105,20 +108,24 @@ export default function Input() {
           type="text"
           value={input}
           onChange={inputChange}
+          onKeyDown={(e) => {
+            // 엔터키 전송
+            e.key === 'Enter' && sendMessage(e);
+          }}
           placeholder="Send a message"
         />
-        <Stbutton onClick={sendMessage}>
-          <svg
+        <Stbutton isInput={isInput} onClick={sendMessage} disabled={!isInput}>
+          <Stsvg
             xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
             className="bi bi-send"
             viewBox="-1.5 0 20 15"
+            isInput={isInput}
           >
             <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z" />
-          </svg>
+          </Stsvg>
         </Stbutton>
       </Stdiv>
+
       <div>{response}</div>
     </>
   );
