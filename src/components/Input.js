dd6f999275b0b282aa46e014b01252fd67025e2a
jaejae990921 <jaejae990921@naver.com>
2023-10-12 12:04:05 +0900

    remove .env file from git

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,13 +1,47 @@
 import { useState } from 'react';
+import styled from 'styled-components';
+
+const Stdiv = styled.div`
+  background-color: #353540;
+  border-radius: 15px;
+  display: flex;
+  width: 45%;
+  height: 60px;
+  padding: 0 10px;
+  align-items: center;
+  justify-content: space-between;
+  position: absolute;
+  bottom: 5%;
+  left: 30%;
+`;
+
+const Stinput = styled.input`
+  width: 100%;
+  border-radius: 15px;
+  background-color: transparent;
+  color: white;
+  border: none;
+  outline: none;
+  font-size: 15px;
+`;
+
+let isInput = false;
+
+const Stbutton = styled.button`
+  border: none;
+  border-radius: 10px;
+  width: 35px;
+  height: 35px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  /* background-color: ${isInput ? 'rgb(25, 195, 125)' : 'transparent'}; */
+  background-color: rgb(25, 195, 125);
+`;
 
 export default function Input() {
   const [input, setInput] = useState('');
   const [response, setResponse] = useState('');
-  let isInput = false;
-
-  const BtnSt = {
-    backgroundColor: isInput ? 'rgb(25, 195, 125)' : 'transparent',
-  };
 
   const API_KEY = '123';
   // const API_KEY = process.env.REACT_APP_API_KEY;
@@ -48,64 +82,43 @@ export default function Input() {
     setInput('');
   };
 
+  // input 값 변경 및 isInput 값 변경
+  const inputChange = (e) => {
+    setInput(e.target.value);
+    if (e.target.value.length == 0) {
+      isInput = false;
+    } else {
+      isInput = true;
+    }
+    isInputChange();
+  };
+
+  const isInputChange = () => {
+    // 색 변경
+    console.log(isInput);
+  };
+
   return (
     <>
-      <div
-        style={{
-          backgroundColor: '#353540',
-          borderRadius: '15px',
-          display: 'flex',
-          width: '60%',
-          height: '60px',
-          padding: '0 10px',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-          position: 'absolute',
-          bottom: '5%',
-          left: '25%',
-        }}
-      >
-        <input
-          style={{
-            width: '100%',
-            borderRadius: '15px',
-            backgroundColor: 'transparent',
-            color: 'white',
-            border: 'none',
-            outline: 'none',
-            fontSize: '15px',
-          }}
+      <Stdiv>
+        <Stinput
           type="text"
           value={input}
-          onChange={(e) => setInput(e.target.value)}
+          onChange={inputChange}
           placeholder="Send a message"
         />
-        <button
-          onClick={sendMessage}
-          style={{
-            border: 'none',
-            borderRadius: '10px',
-            width: '35px',
-            height: '35px',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            backgroundColor: 'rgb(25, 195, 125)',
-          }}
-        >
+        <Stbutton onClick={sendMessage}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="20"
             height="20"
-            fill="currentColor"
-            class="bi bi-send"
+            className="bi bi-send"
             viewBox="-1.5 0 20 15"
-            color="white"
           >
             <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z" />
           </svg>
-        </button>
-      </div>
+        </Stbutton>
+      </Stdiv>
       <div>{response}</div>
     </>
   );
