46b1b9a6e49d086966d7b4dda0cabaee24cb6fef
jaejae990921 <jaejae990921@naver.com>
2023-10-13 00:00:38 +0900

    design - 메인 preview a 태그

---
+++ src/components/Preview.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+const Stdiv = styled.div`
+  font-size: 12px;
+  color: #c5c5d1;
+  position: absolute;
+  bottom: 10px;
+`;
+
+export default function Preview() {
+  return (
+    <>
+      <Stdiv>
+        Free Research Preview. ChatGPT may produce inaccurate information about
+        people, places, or facts.{' '}
+        <a
+          href="https://help.openai.com/en/articles/6825453-chatgpt-release-notes"
+          target="_blank"
+          style={{ color: '#c5c5d1' }}
+        >
+          ChatGPT September 25 Version
+        </a>
+      </Stdiv>
+    </>
+  );
+}
