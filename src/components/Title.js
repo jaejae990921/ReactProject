27d02ac244b7f1a8d251b82bf0b3d0c4a5a48329
jaejae990921 <jaejae990921@naver.com>
2023-10-13 00:00:25 +0900

    design - 메인 title div

---
+++ src/components/Title.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const Stdiv = styled.div`
+  font-size: 36px;
+  color: #575867;
+  font-weight: bold;
+  background-color: transparent;
+`;
+
+export default function Title() {
+  return (
+    <>
+      <Stdiv>ChatGPT</Stdiv>
+    </>
+  );
+}
