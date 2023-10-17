9cbfd4f18bb5d01c6c9343b6aecb11c9426d4dca
jaejae990921 <jaejae990921@naver.com>
2023-10-17 23:59:27 +0900

    design - Menu 하단 div 2개 추가

---
+++ src/components/Up.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+import { FiUser } from 'react-icons/fi';
+
+const StDiv = styled.div`
+  width: 95%;
+  background-color: transparent;
+  height: 50%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  box-sizing: border-box;
+  padding: 10px;
+  border-radius: 10px;
+  transition: all 0.2s;
+
+  &:hover {
+    background-color: rgb(52, 53, 65);
+  }
+`;
+
+const UpgradeDiv = styled.div`
+  font-size: 13px;
+  font-weight: bold;
+  color: white;
+  box-sizing: border-box;
+  padding-left: 10px;
+`;
+
+export default function Up() {
+  return (
+    <>
+      <StDiv>
+        <FiUser style={{ width: '16', height: '16' }}></FiUser>
+        <UpgradeDiv>Upgrade to Plus</UpgradeDiv>
+      </StDiv>
+    </>
+  );
+}
---
+++ src/components/User.js
@@ -0,0 +1,50 @@
+import styled from 'styled-components';
+import userImg from '../assets/user.png';
+import { BsThreeDots } from 'react-icons/bs';
+
+const StDiv = styled.div`
+  width: 95%;
+  background-color: transparent;
+  height: 50%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  box-sizing: border-box;
+  padding: 10px;
+  border-radius: 10px;
+  transition: all 0.2s;
+  position: relative;
+
+  &:hover {
+    background-color: rgb(52, 53, 65);
+  }
+`;
+
+const UpgradeDiv = styled.div`
+  font-size: 13px;
+  font-weight: bold;
+  color: white;
+  box-sizing: border-box;
+  padding-left: 10px;
+`;
+
+const Stimg = styled.img`
+  width: 32px;
+  height: 32px;
+  border-radius: 3px;
+`;
+
+export default function Up() {
+  return (
+    <>
+      <StDiv>
+        <Stimg src={userImg} />
+        <UpgradeDiv>User</UpgradeDiv>
+        <BsThreeDots
+          fill="rgb(143, 142, 158)"
+          style={{ position: 'absolute', right: '5px' }}
+        />
+      </StDiv>
+    </>
+  );
+}
